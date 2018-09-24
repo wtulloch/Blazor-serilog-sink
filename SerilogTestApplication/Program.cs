cd7c409e32400c1bea5520c5c8b21b01c1c20184
William Tulloch <william.tulloch@readify.net>
2018-09-24 17:34:16 +1000

    added sample application

---
+++ SerilogTestApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using Blazor.Serilog.Sink;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Serilog;
+
+namespace SerilogTestApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.BrowserConsoleSink(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] {Message:lj} {SourceContext}{NewLine}{Exception}"
+                )
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
